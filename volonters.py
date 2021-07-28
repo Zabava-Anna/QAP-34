52ece5103ab407c543500194da771a2c3b4bd68f
Zabava-Anna <85166283+Zabava-Anna@users.noreply.github.com>
2021-07-28 23:27:11 +0300

    Create volonters.py

---
+++ volonters.py
@@ -0,0 +1,17 @@
+class Volonter:
+    def __init__(self, name=str, lastname = str, city=str, status=str):
+        self.name = name
+        self.lastname = lastname
+        self.city = city
+        self.status = status
+    def about(self):
+        return "{0} {1}, {2}, статус '{3}'".format(self.name, self.lastname, self.city, self.status)
+
+pers1 = Volonter(input("введите Имя \n"), input("Введите Фамилию \n"), input("Введите Городо \n"), input("Ваш статус \n"))
+pers2 = Volonter(input("введите Имя \n"), input("Введите Фамилию \n"), input("Введите Городо \n"), input("Ваш статус \n"))
+pers3 = Volonter(input("введите Имя \n"), input("Введите Фамилию \n"), input("Введите Городо \n"), input("Ваш статус \n"))
+
+
+Volonters = [pers1, pers2, pers3]
+for pers in Volonters:
+    print(pers.about())
